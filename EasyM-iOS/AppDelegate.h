0a4ad8a4c5b5357357b3204e1df33b6662f342c3
SeanCST <seancst.x@gmail.com>
2019-01-04 12:13:54 +0800

    Initial Commit

---
+++ EasyM-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyM-iOS
+//
+//  Created by SeanCST on 2019/1/4.
+//  Copyright © 2019 NilOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyM-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyM-iOS
+//
+//  Created by SeanCST on 2019/1/4.
+//  Copyright © 2019 NilOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
